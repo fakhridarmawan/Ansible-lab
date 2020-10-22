842f6d09ba982ffaff6c8fdbc1b05b3635e619f5
pujiriawan <62480172+pujiriawan@users.noreply.github.com>
2020-10-22 16:51:02 +0700

    Menambahkan module provsioner run shell script

--- main.tf
+++ main.tf
@@ -3,18 +3,29 @@ provider "azurerm" {
   features {}
 }
 
+#Membaut Resource Group
 resource "azurerm_resource_group" "main" {
   name = "ansible-lab"
   location = "southeastasia"
+  tags = {
+    environment = "LAB ANSIBLE"
+  }
 }
 
+#Membuat Virtual Network di dalam Cloud Azure
 resource "azurerm_virtual_network" "main" {
   name = "${var.prefix}-network"
   address_space = ["10.0.0.0/16"]
   location = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
+
+tags = {
+    environment = "LAB ANSIBLE"
+  }
+
 }
 
+#Membuat subnetwork di dalam internal cloud azure milik kita
 resource "azurerm_subnet" "internal" {
   name = "internal"
   resource_group_name = azurerm_resource_group.main.name
@@ -22,19 +33,50 @@ resource "azurerm_subnet" "internal" {
   address_prefixes = ["10.0.3.0/24"]
 }
 
+# Membuat dan mengassign publik ip ke VM yang di provisioning supaya dapat di akses dari internet
 resource "azurerm_public_ip" "pip" {
   count = 5
   name = "${var.prefix}-pip-${count.index}"
   resource_group_name = azurerm_resource_group.main.name
   location = azurerm_resource_group.main.location
-  allocation_method = "Dynamic"
+  allocation_method = "Static"
+
+  tags = {
+    environment = "LAB ANSIBLE"
+  }
 }
+
+# Membuat security group untuk membuka hanya akses ssh 
+resource "azurerm_network_security_group" "akses-ssh" {
+    name                = "Firewall-SecurityGroups"
+    location            = "southeastasia"
+    resource_group_name = azurerm_resource_group.main.name
+
+    security_rule {
+        name                       = "SSH"
+        priority                   = 1001
+        direction                  = "Inbound"
+        access                     = "Allow"
+        protocol                   = "Tcp"
+        source_port_range          = "*"
+        destination_port_range     = "22"
+        source_address_prefix      = "*"
+        destination_address_prefix = "*"
+        
+    }
+
+    tags = {
+        environment = "LAB ANSIBLE"
+    }
+}
+
 
 resource "azurerm_network_interface" "main" {
   count               = 5
   name                = "${var.prefix}-nic-vm${count.index}"
   resource_group_name = azurerm_resource_group.main.name
   location            = azurerm_resource_group.main.location
+  
 
   ip_configuration {
     name                          = "primary"
@@ -42,6 +84,17 @@ resource "azurerm_network_interface" "main" {
     private_ip_address_allocation = "Dynamic"
     public_ip_address_id          = element(azurerm_public_ip.pip.*.id, count.index)
   }
+  
+  tags = {
+        environment = "LAB ANSIBLE"
+    }
+}
+
+# Binding security group ke network interface yang sudah dibuat di atas.
+resource "azurerm_network_interface_security_group_association" "bind-akses-ssh" {
+    count = 5
+    network_interface_id      = element(azurerm_network_interface.main.*.id, count.index)
+    network_security_group_id = azurerm_network_security_group.akses-ssh.id
 }
 
 resource "azurerm_linux_virtual_machine" "main" {
@@ -66,4 +119,35 @@ resource "azurerm_linux_virtual_machine" "main" {
     caching = "ReadWrite"
     storage_account_type = "Standard_LRS"
   }
+
+    provisioner "file" {
+   source = "yum-update.sh"
+   destination = "/tmp/yum-update.sh"
+   
+   connection {
+			host = element(azurerm_public_ip.pip.*.ip_address, count.index)
+			type	= "ssh"
+			user	= "fakhridarmawan"
+			password = "P@ssw0rd.12345"
+      }  
+}  
+
+
+provisioner "remote-exec" {
+    inline = [
+	"chmod +x /tmp/yum-update.sh",
+  "sudo /tmp/yum-update.sh args"   
+   ]
+
+   connection {
+			host = element(azurerm_public_ip.pip.*.ip_address, count.index)
+			type	= "ssh"
+			user	= "fakhridarmawan"
+			password = "P@ssw0rd.12345"
+			}  
+} 
+
+ 
+
 }
+
